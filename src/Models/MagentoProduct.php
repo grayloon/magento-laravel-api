7713f6f0bfc522b4a6a524644d1ce20dacbc87ce
ahinkle <ahinkle10@gmail.com>
2020-07-10 08:54:59 -0500

    chore: add mutatable dates to use with carbon

--- src/Models/MagentoProduct.php
+++ src/Models/MagentoProduct.php
@@ -28,6 +28,13 @@ class MagentoProduct extends Model
     protected $guarded = [];
 
     /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at', 'synced_at'];
+
+    /**
      * The Magento Product has many Ext Attributes.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
