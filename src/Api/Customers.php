93d8c06c7b9bdb115fc3084a31b961b9434fbfcf
ahinkle <ahinkle10@gmail.com>
2020-07-23 12:16:09 -0500

    chore: ability to search customers with filter applied

--- src/Api/Customers.php
+++ src/Api/Customers.php
@@ -7,16 +7,17 @@ class Customers extends AbstractApi
     /**
      * The list of customers.
      *
-     * @param int $pageSize
-     * @param int $currentPage
+     * @param  int  $pageSize
+     * @param  int  $currentPage
+     * @param  array  $filters
      *
      * @return  array
      */
-    public function all($pageSize = 50, $currentPage = 1)
+    public function all($pageSize = 50, $currentPage = 1, $filters = [])
     {
-        return $this->get('/customers/search', [
+        return $this->get('/customers/search', array_merge($filters, [
             'searchCriteria[pageSize]'    => $pageSize,
             'searchCriteria[currentPage]' => $currentPage,
-        ]);
+        ]));
     }
 }
--- tests/Api/CustomersTest.php
+++ tests/Api/CustomersTest.php
@@ -23,4 +23,16 @@ class CustomersTest extends TestCase
 
         $this->assertNull($magento->api('customers')->all());
     }
+
+    public function test_can_call_magento_api_customers_all_with_filter()
+    {
+        Http::fake();
+
+        $magento = new Magento();
+
+        $this->assertNull($magento->api('customers')->all(1, 1, [
+            'searchCriteria[filterGroups][0][filters][0][field]' => 'email',
+            'searchCriteria[filterGroups][0][filters][0][value]' => 'foo@bar.com',
+        ]));
+    }
 }
