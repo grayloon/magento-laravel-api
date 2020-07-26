24296fdaee48a72ce1b2a7a862b334cfe1295cac
ahinkle <ahinkle10@gmail.com>
2020-07-25 21:41:19 -0500

    chore: remove unused properties

--- src/Console/SyncMagnetoProductsCommand.php
+++ src/Console/SyncMagnetoProductsCommand.php
@@ -15,41 +15,6 @@ class SyncMagnetoProductsCommand extends Command
     protected $signature = 'magento:sync-products';
 
     /**
-     * The progress bar to display to the user.
-     *
-     * @var object
-     */
-    public $bar;
-
-    /**
-     * The total count of records found.
-     *
-     * @var int
-     */
-    public $totalCount = 0;
-
-    /**
-     * The amount of records per API request.
-     *
-     * @var int
-     */
-    public $pageSize = 50;
-
-    /**
-     * The paginated page to request.
-     *
-     * @var int
-     */
-    public $currentPage = 1;
-
-    /**
-     * The total amount of pages to paginate.
-     *
-     * @var int
-     */
-    public $totalPages = 1;
-
-    /**
      * The console command description.
      *
      * @var string
