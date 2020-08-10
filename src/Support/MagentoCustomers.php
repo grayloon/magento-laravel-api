9807ceca84fa8a90a5bdfe2fa0620599fd32284b
ahinkle <ahinkle10@gmail.com>
2020-08-10 14:40:11 -0500

    feat: ability to resolve custom attributes from customers

--- src/Support/MagentoCustomers.php
+++ src/Support/MagentoCustomers.php
@@ -4,6 +4,7 @@ namespace Grayloon\Magento\Support;
 
 use Grayloon\Magento\Magento;
 use Grayloon\Magento\Models\MagentoCustomer;
+use Grayloon\Magento\Models\MagentoCustomAttributeType;
 
 class MagentoCustomers extends PaginatableMagentoService
 {
@@ -82,16 +83,68 @@ class MagentoCustomers extends PaginatableMagentoService
         }
 
         foreach ($attributes as $attribute) {
-            if (is_array($attribute['value'])) {
-                $attribute['value'] = json_encode($attribute['value']);
-            }
+            $type = $this->resolveCustomAttributeType($attribute['attribute_code']);
+            $value = $this->resolveCustomAttributeValue($type, $attribute['value']);
 
-            $customer->customAttributes()->updateOrCreate(['attribute_type' => $attribute['attribute_code']], [
-                'value' => $attribute['value'] ?? '',
-            ]);
+            $customer
+                ->customAttributes()
+                ->updateOrCreate(['attribute_type_id' => $type->id], [
+                    'attribute_type' => $attribute['attribute_code'],
+                    'value'          => $value,
+                ]);
         }
 
         return $this;
+    }
+
+    /**
+     * Resolve the Custom Attribute Type by the Attribute Code.
+     *
+     * @param  string  $attributeCode
+     * @return \Grayloon\Magento\Models\MagentoCustomAttributeType
+     */
+    protected function resolveCustomAttributeType($attributeCode)
+    {
+        $type = MagentoCustomAttributeType::where('name', $attributeCode)
+            ->first();
+
+        if (! $type) {
+            $api = (new Magento())->api('productAttributes')
+                ->show($attributeCode)
+                ->json();
+
+            $type = MagentoCustomAttributeType::create([
+                'name'         => $attributeCode,
+                'display_name' => $api['default_frontend_label'] ?? $attributeCode,
+                'options'      => $api['options'] ?? [],
+            ]);
+        }
+
+        return $type;
+    }
+
+    /**
+     * Resolve the Custom Attribute Value by the provided options.
+     *
+     * @param  \Grayloon\Magento\Models\MagentoCustomAttributeType  $type
+     * @param  string  $value;
+     * @return string|null
+     */
+    protected function resolveCustomAttributeValue($type, $value)
+    {
+        if ($type->options) {
+            foreach ($type->options as $option) {
+                if ($option['value'] == $value) {
+                    return $option['label'];
+                }
+            }
+        }
+
+        if (is_array($value)) {
+            $value = json_encode($value);
+        }
+
+        return $value;
     }
 
     /**
--- tests/Support/MagentoCustomersTest.php
+++ tests/Support/MagentoCustomersTest.php
@@ -2,10 +2,11 @@
 
 namespace Grayloon\Magento\Tests\Support;
 
-use Grayloon\Magento\Models\MagentoCustomer;
-use Grayloon\Magento\Support\MagentoCustomers;
 use Grayloon\Magento\Tests\TestCase;
 use Illuminate\Support\Facades\Http;
+use Grayloon\Magento\Models\MagentoCustomer;
+use Grayloon\Magento\Support\MagentoCustomers;
+use Grayloon\Magento\Models\MagentoCustomAttributeType;
 
 class MagentoCustomersTest extends TestCase
 {
@@ -26,6 +27,8 @@ class MagentoCustomersTest extends TestCase
 
     public function test_can_create_magento_customer()
     {
+        factory(MagentoCustomAttributeType::class)->create(['name' => 'rewards_member']);
+
         $customers = [
             [
                 'id'         => '1',
@@ -78,5 +81,75 @@ class MagentoCustomersTest extends TestCase
         $this->assertEquals('100 Beetfarm Lake', $customer->addresses()->first()->street);
         $this->assertNotEmpty($customer->customAttributes()->get());
         $this->assertEquals('rewards_member', $customer->customAttributes()->first()->attribute_type);
+    }
+
+    public function test_can_apply_new_custom_attribute_type_to_customer()
+    {
+        Http::fake(function ($request) {
+            return Http::response([
+                'options' => [
+                    [
+                        "label" => 'Yes',
+                        'value' => '1',
+                    ],
+                    [
+                        'label' => 'No',
+                        'value' => '0',
+                    ]
+                ],
+                'default_frontend_label' => 'Rewards Member',
+            ], 200);
+        });
+
+        $customers = [
+            [
+                'id'         => '1',
+                'group_id'   => '1',
+                'created_at' => '2014-04-04 14:17:29',
+                'updated_at' => '2014-04-04 14:17:29',
+                'email'      => 'dschrute@dundermifflin.com',
+                'firstname'  => 'Dwight',
+                'lastname'   => 'Schrute',
+                'store_id'   => 1,
+                'website_id' => 1,
+                'addresses' => [
+                    [
+                        'id' => 1,
+                        'customer_id' => 1,
+                        'region' => [
+                            'region_code' => 'PA',
+                            'region'      => 'Pennsylvania',
+                            'region_id'   => 1,
+                        ],
+                        'region_id' => 1,
+                        'country_id' => 'US',
+                        'street' => [
+                            '100 Beetfarm Lake',
+                        ],
+                        'telephone' => '888 888-8888',
+                        'city'      => 'Scranton',
+                        'firstname' => 'Dwight',
+                        'lastname'  => 'Schrute',
+                    ],
+                ],
+                'custom_attributes' => [
+                    [
+                        'attribute_code' => 'rewards_member',
+                        'value'          => '1',
+                    ],
+                ],
+            ],
+        ];
+
+        $magentoCustomers = new MagentoCustomers();
+
+        $magentoCustomers->updateCustomers($customers);
+
+        $customer = MagentoCustomer::first();
+
+        $this->assertNotEmpty($customer->customAttributes()->get());
+        $this->assertEquals('rewards_member', $customer->customAttributes()->first()->attribute_type);
+        $this->assertInstanceOf(MagentoCustomAttributeType::class, $customer->customAttributes()->first()->type()->first());
+        $this->assertEquals('Rewards Member', $customer->customAttributes()->first()->type()->first()->display_name);
     }
 }
