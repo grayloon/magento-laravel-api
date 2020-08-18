663e128634791a249d83655c62d60ee787e4a5c9
ahinkle <ahinkle10@gmail.com>
2020-08-18 13:09:49 -0500

    refactor: remove storage of product link types

--- src/MagentoServiceProvider.php
+++ src/MagentoServiceProvider.php
@@ -74,7 +74,6 @@ class MagentoServiceProvider extends ServiceProvider
             Console\SyncMagnetoProductsCommand::class,
             Console\SyncMagnetoCategoriesCommand::class,
             Console\SyncMagnetoCustomersCommand::class,
-            Console\SyncMagentoProductLinkTypesCommand::class,
         ]);
 
         // Register the main class to use with the facade
