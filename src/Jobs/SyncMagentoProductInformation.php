4491c5bf46b2a9f83e47f8fe0bd7aa890237dbd9
ahinkle <ahinkle10@gmail.com>
2020-08-18 14:08:30 -0500

    feat: store related product links (wip)

---
+++ src/Jobs/SyncMagentoProductInformation.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Grayloon\Magento\Jobs;
+
+use Grayloon\Magento\Magento;
+use Grayloon\Magento\Models\MagentoProduct;
+use Grayloon\Magento\Support\MagentoProductLinks;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SyncMagentoProductInformation implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The Magento Product.
+     *
+     * @var \Grayloon\Magento\Models\MagentoProduct  $product
+     */
+    public $product;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param  \Grayloon\Magento\Models\MagentoProduct  $product
+     * @return void
+     */
+    public function __construct(MagentoProduct $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $productApi = (new Magento())->api('products')
+            ->show($this->product->sku)
+            ->json();
+
+        (new MagentoProductLinks())->updateProductLinks($this->product, $productApi);
+    }
+}
---
+++ src/Support/MagentoProductLinks.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Grayloon\Magento\Support;
+
+use Grayloon\Magento\Jobs\SyncMagentoProductInformation;
+use Grayloon\Magento\Models\MagentoProduct;
+use Grayloon\Magento\Models\MagentoProductLink;
+
+class MagentoProductLinks
+{
+    /**
+     * Updates the product links from the Api response.
+     *
+     * @param  Grayloon\Magento\Models\MagentoProduct  $product
+     * @param  array  $response
+     * @return void
+     */
+    public function updateProductLinks($product, $response)
+    {
+        if (! $response || ! isset($response['product_links']) || ! $response['product_links']) {
+            return;
+        }
+
+        foreach ($response['product_links'] as $link) {
+            $productLink = MagentoProduct::where('sku', $link['sku'])->first();
+
+            // If the relating product doesn't exist yet, relaunch the job to check again later.
+            if (! $productLink) {
+                SyncMagentoProductInformation::dispatch($product);
+                continue;
+            }
+
+            MagentoProductLink::updateOrCreate([
+                'product_id', $product->id,
+                'related_product_id', $productLink->id,
+                'link_type', $link['link_type'],
+            ], [
+                'position' => $link['position'],
+            ]);
+        }
+    }
+}
