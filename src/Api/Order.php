e3737c0a5f0b93bcb71176ff3b408a8d09a1f24d
ASUS <coverterror@gmail.com>
2021-09-22 22:55:15 +0300

    formatting fix

--- src/Api/Order.php
+++ src/Api/Order.php
@@ -10,14 +10,14 @@ class Order extends AbstractApi
     /**
      * Creates invoice for order.
      *
-     * @param int $orderId
-     * @param bool $capture
-     * @param bool $notify
-     * @param bool $appendComment
-     * @param string $extension_attributes
-     * @param string $comment
-     * @param int $is_visible_on_front
-     * @return  Response
+     * @param  int  $orderId
+     * @param  bool  $capture
+     * @param  bool  $notify
+     * @param  bool  $appendComment
+     * @param  string  $extension_attributes
+     * @param  string  $comment
+     * @param  int  $is_visible_on_front
+     * @return Response
      */
     public function invoice(int $orderId, bool $capture = true, bool $notify = true, bool $appendComment = true,
                             string $extension_attributes = '', string $comment = '', int $is_visible_on_front = 0): Response
