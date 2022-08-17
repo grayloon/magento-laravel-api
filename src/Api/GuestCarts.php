5dba3908f832d513802267d2ba4b0ce158e87549
Hamza Alayed <developerh108@gmail.com>
2022-08-17 17:13:24 +0300

    add new function for create quote link

--- src/Api/GuestCarts.php
+++ src/Api/GuestCarts.php
@@ -40,9 +40,9 @@ class GuestCarts extends AbstractApi
     /**
      * Add/update the specified cart item.
      *
-     * @param  string  $cartId
-     * @param  string  $sku
-     * @param  string  $quantity
+     * @param string $cartId
+     * @param string $sku
+     * @param string $quantity
      * @return array
      */
     public function addItem($cartId, $sku, $quantity)
@@ -50,8 +50,8 @@ class GuestCarts extends AbstractApi
         return $this->post('/guest-carts/' . $cartId . '/items', [
             'cartItem' => [
                 'quote_id' => $cartId,
-                'sku'      => $sku,
-                'qty'      => $quantity
+                'sku' => $sku,
+                'qty' => $quantity
             ],
         ]);
     }
@@ -60,8 +60,8 @@ class GuestCarts extends AbstractApi
      * Add/update the specified cart item with product options.
      * https://magento.redoc.ly/2.4.4-customer/tag/guest-cartscartIditems#operation/quoteGuestCartItemRepositoryV1SavePost
      *
-     * @param  string  $cartId
-     * @param  array   $array
+     * @param string $cartId
+     * @param array $array
      * @return array
      */
     public function addItemWithOptions($cartId, $body = [])
@@ -73,9 +73,9 @@ class GuestCarts extends AbstractApi
     /**
      * Add/update the specified cart item.
      *
-     * @param  string  $cartId
-     * @param  int  $itemId
-     * @param  array  $body
+     * @param string $cartId
+     * @param int $itemId
+     * @param array $body
      * @return array
      */
     public function editItem($cartId, $itemId, $body = [])
@@ -96,8 +96,8 @@ class GuestCarts extends AbstractApi
     /**
      * Estimate shipping by address and return list of available shipping methods.
      *
-     * @param  string  $cartId
-     * @param  array  $body
+     * @param string $cartId
+     * @param array $body
      * @return array
      */
     public function estimateShippingMethods($cartId, $body = [])
@@ -108,8 +108,8 @@ class GuestCarts extends AbstractApi
     /**
      * Estimate shipping by address and return list of available shipping methods.
      *
-     * @param  string  $cartId
-     * @param  array  $body
+     * @param string $cartId
+     * @param array $body
      * @return array
      */
     public function shippingMethods($cartId)
@@ -134,8 +134,8 @@ class GuestCarts extends AbstractApi
     /**
      * Calculate quote totals based on address and shipping method.
      *
-     * @param  string  $cartId
-     * @param  array  $body
+     * @param string $cartId
+     * @param array $body
      * @return array
      */
     public function totalsInformation($cartId, $body = [])
@@ -146,8 +146,8 @@ class GuestCarts extends AbstractApi
     /**
      * Save the total shipping information.
      *
-     * @param  string  $cartId
-     * @param  array  $body
+     * @param string $cartId
+     * @param array $body
      * @return array
      */
     public function shippingInformation($cartId, $body = [])
@@ -158,7 +158,7 @@ class GuestCarts extends AbstractApi
     /**
      * List available payment methods for a specified shopping cart. This call returns an array of objects, but detailed information about each object’s attributes might not be included.
      *
-     * @param  string  $cartId
+     * @param string $cartId
      * @return array
      */
     public function paymentMethods($cartId)
@@ -169,8 +169,8 @@ class GuestCarts extends AbstractApi
     /**
      * Set payment information and place order for a specified cart.
      *
-     * @param  string  $cartId
-     * @param  array  $body
+     * @param string $cartId
+     * @param array $body
      * @return array
      */
     public function paymentInformation($cartId, $body = [])
@@ -181,8 +181,8 @@ class GuestCarts extends AbstractApi
     /**
      * Remove the specified item from the specified cart.
      *
-     * @param  string  $cartId
-     * @param  int  $itemId
+     * @param string $cartId
+     * @param int $itemId
      * @return array
      */
     public function removeItem($cartId, $itemId)
@@ -193,23 +193,31 @@ class GuestCarts extends AbstractApi
     /**
      * Get view quote link
      *
-     * @param  string  $cartId
-     * @param  string  $adminEmail
+     * @param string $cartId
+     * @param string $adminEmail
+     * @param string $customerEmail
+     * @param string $customerFirstName
+     * @param string $customerLastName
      * @return Response
+     * @throws Exception
      */
-    public function quoteLink($cartId, $adminEmail)
+    public function quoteLink( $cartId, $adminEmail, $customerEmail, $customerFirstName,  $customerLastName): Response
     {
-        return $this->post('/guest-carts/quoteLink',[
-            'cartId'=>$cartId,
-            'adminUserEmail'=>$adminEmail,
-        ]);
+        return $this->post('/guest-carts/quoteLink', [
+            'cartId' => $cartId,
+            'adminUserEmail' => $adminEmail,
+            'customerEmail' => $customerEmail,
+            'customerFirstName' => $customerFirstName,
+            'customerLastName' => $customerLastName,
+        ]
+        );
     }
 
     /**
      * Apply a coupon to a specified cart.
      *
-     * @param  string  $cartId
-     * @param  string  $couponCode
+     * @param string $cartId
+     * @param string $couponCode
      * @return void
      */
     public function couponCode($cartId, $couponCode)
@@ -230,9 +238,9 @@ class GuestCarts extends AbstractApi
     /**
      * Assign a specified customer to a specified shopping cart.
      *
-     * @param  string  $cartId
-     * @param  int  $customerId
-     * @param  int  $storeId
+     * @param string $cartId
+     * @param int $customerId
+     * @param int $storeId
      * @return array
      */
     public function assignCustomer($cartId, $customerId, $storeId)
